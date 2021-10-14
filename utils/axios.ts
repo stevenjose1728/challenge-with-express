b4686d8cda9d75db265afad0cf4d3e803c1ce933
Steven <stevenvarela98@gmail.com>
2021-10-14 08:54:11 -0400

    :wrench: global file to use axios with configs

---
+++ utils/axios.ts
@@ -0,0 +1,27 @@
+import axios, {AxiosError, AxiosResponse ,AxiosInstance} from 'axios';
+// import {store} from 'store'
+const baseURL = process.env.NEXT_API_URL;
+
+let _axios: AxiosInstance = axios.create({
+  baseURL,
+});
+_axios.interceptors.request.use(
+  (config) => {
+    // const { user } = store.getState();
+    // if (user && user.token) {
+    //   config.headers["Authorization"] = `Bearer ${user.token}`;
+    // }
+
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+_axios.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  }
+);
+export default _axios;
