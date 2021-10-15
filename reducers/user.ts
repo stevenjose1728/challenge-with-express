d4902e7ece21645fab307d28d3bfd03b249989a5
Steven <stevenvarela98@gmail.com>
2021-10-15 16:40:23 -0400

    :construction: user reducer

--- reducers/user.ts
+++ reducers/user.ts
@@ -1,13 +1,13 @@
 import {User} from '../models'
 type Action = {
     payload: User | null,
-    type: 'SET_PLAN' | 'REMOVE_PLAN'
+    type: 'SET_USER' | 'REMOVE_USER'
 }
 function reducer(state: User | null = null, action: Action): User | null {
 	switch(action.type) {
-		case 'SET_PLAN':
+		case 'SET_USER':
             return action.payload;
-        case 'REMOVE_PLAN':
+        case 'REMOVE_USER':
             return null;
         default:
         	return state;
